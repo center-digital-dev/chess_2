4fd35459a2c49a1116715234f88a84f1352e21c5
Denis Koshelev <deniskos200029@gmail.com>
2025-01-23 15:52:37 +0300

    Подключил eslint для отображения ошибок в коде

--- src/views/main/Home.tsx
+++ src/views/main/Home.tsx
@@ -1,17 +1,11 @@
-import styles from "./page.module.css";
 import Image from "next/image";
+
+import styles from "./page.module.css";
 const HomePage = () => {
    return (
       <div className={styles.page}>
          <main className={styles.main}>
-            <Image
-               className={styles.logo}
-               src="/next.svg"
-               alt="Next.js logo"
-               width={180}
-               height={38}
-               priority
-            />
+            <Image className={styles.logo} src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
             <ol>
                <li>
                   Get started by editing <code>src/app/page.tsx</code>.
@@ -26,13 +20,7 @@ const HomePage = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                >
-                  <Image
-                     className={styles.logo}
-                     src="/vercel.svg"
-                     alt="Vercel logomark"
-                     width={20}
-                     height={20}
-                  />
+                  <Image className={styles.logo} src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
                   Deploy now
                </a>
                <a href="/article" className={styles.secondary}>
@@ -42,13 +30,7 @@ const HomePage = () => {
          </main>
          <footer className={styles.footer}>
             <a href="/history">
-               <Image
-                  aria-hidden
-                  src="/file.svg"
-                  alt="File icon"
-                  width={16}
-                  height={16}
-               />
+               <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
                Learn
             </a>
             <a
@@ -56,13 +38,7 @@ const HomePage = () => {
                target="_blank"
                rel="noopener noreferrer"
             >
-               <Image
-                  aria-hidden
-                  src="/window.svg"
-                  alt="Window icon"
-                  width={16}
-                  height={16}
-               />
+               <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
                Examples
             </a>
             <a
@@ -70,13 +46,7 @@ const HomePage = () => {
                target="_blank"
                rel="noopener noreferrer"
             >
-               <Image
-                  aria-hidden
-                  src="/globe.svg"
-                  alt="Globe icon"
-                  width={16}
-                  height={16}
-               />
+               <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
                Go to nextjs.org →
             </a>
          </footer>

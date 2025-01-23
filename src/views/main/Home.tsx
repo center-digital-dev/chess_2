2b72d35dd433b70be07796f23049ecfb60e9d8f7
Denis Koshelev <deniskos200029@gmail.com>
2025-01-23 18:04:32 +0300

    Подключил styleling для форматирование scss свойств

--- src/views/main/Home.tsx
+++ src/views/main/Home.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 const HomePage = () => {
    return (
       <div className={styles.page}>
